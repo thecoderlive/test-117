dad7f1192f377a79d1a33ea8c5272b5874d0d3d0
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-04 09:58:54 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "0b454279-fe8d-496e-9876-1e12e347b134",
+    "home_feed": [
+        {
+            "id": "bdfac72f-802c-474d-9e9b-06740396b21f",
+            "artist_name": "about",
+            "home_carousel": [
+                {
+                    "id": "698c1bc8-7ba6-4ace-bde9-748c639f8997",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/228.png"
+                },
+                {
+                    "id": "8bac67ae-10b5-4a74-99dd-95b11dc93c69",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png"
+                },
+                {
+                    "id": "0b0c38ca-3079-4fee-afe5-369132f40266",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/281.png"
+                },
+                {
+                    "id": "51e6ec31-2b8f-426e-b3ce-43469cd64b73",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+                },
+                {
+                    "id": "8a35c8cc-c047-46a1-b7bb-9715a9952736",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/362.png"
+                }
+            ],
+            "art_caption": "lot",
+            "art_description": "Since parent put. Rich describe top region strong party decision."
+        },
+        {
+            "id": "41aaaf6e-cd22-4129-84b4-b610c798cee1",
+            "artist_name": "citizen",
+            "home_carousel": [
+                {
+                    "id": "b59a48f3-ef64-4b0e-a6e1-5da7e9f8d1e4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                },
+                {
+                    "id": "53ba82b0-f7ea-451a-86d8-f07101fe023b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/280.png"
+                },
+                {
+                    "id": "b0e4adf8-af56-43b9-906e-490941907abf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                },
+                {
+                    "id": "9379ac70-1521-48bf-8d7c-7922029f1211",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/203.png"
+                },
+                {
+                    "id": "8f790873-0a70-4ef0-b1dd-732a1ac5e3fd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png"
+                }
+            ],
+            "art_caption": "coach",
+            "art_description": "Member red national third relate other development. Country teacher that road anyone. Strategy describe theory happy explain floor."
+        },
+        {
+            "id": "53231ffb-9317-42c0-b054-7fca06b8ebf8",
+            "artist_name": "give",
+            "home_carousel": [
+                {
+                    "id": "c3406c54-145a-464f-bb71-6abd58830f99",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png"
+                },
+                {
+                    "id": "c09fca62-a283-474d-88b6-3b7a56bffac1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png"
+                },
+                {
+                    "id": "cedbdf85-370d-4104-a542-4fdaf0cbee3d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/229.png"
+                },
+                {
+                    "id": "73bba770-4ece-49be-9ed9-9d7dd19034ea",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "f4f6d104-e97f-423e-b896-113da228cdee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/349.png"
+                }
+            ],
+            "art_caption": "imagine",
+            "art_description": "Develop talk hard seek. Leave include fire there. Wind camera network. Expect share threat hundred game speech medical."
+        },
+        {
+            "id": "0042597d-bc8c-483a-b42b-14c1f7c74b48",
+            "artist_name": "officer",
+            "home_carousel": [
+                {
+                    "id": "28a39707-d30b-42fe-b8cd-66e86ac0d812",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                },
+                {
+                    "id": "4de7a962-7c96-4902-ba4d-d702bac589b2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/151.png"
+                },
+                {
+                    "id": "0de79f7c-0aac-4d3e-a30c-f9c66f3da42c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/353.png"
+                },
+                {
+                    "id": "265b6a6a-39d6-4efa-bfd2-333641b901b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                },
+                {
+                    "id": "c54fa376-de1d-4a2b-98c6-f34d023e5a73",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/245.png"
+                }
+            ],
+            "art_caption": "girl",
+            "art_description": "Special property consumer middle four knowledge. At administration most Congress new property."
+        },
+        {
+            "id": "66e8188f-be4a-4e61-986a-7974d44a5c83",
+            "artist_name": "shake",
+            "home_carousel": [
+                {
+                    "id": "23ca3429-0bff-446a-b92e-1086fb80171f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "c3fbccdc-77fa-4195-a1fc-0bb94eec3e0d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png"
+                },
+                {
+                    "id": "e52c4fff-9363-4855-b1c7-076407d2c5ed",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                },
+                {
+                    "id": "2fc13d6b-31b2-460c-be65-87486a21c95f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png"
+                },
+                {
+                    "id": "7f557a5b-febd-41b7-9f9d-163deda67247",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                }
+            ],
+            "art_caption": "money",
+            "art_description": "Professor memory pretty just. Make else sell sound must black safe. Run behavior visit strong class."
+        }
+    ]
+}
